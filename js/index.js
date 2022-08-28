776433172d8f5caf70d25e042df86f7e32463f54
Regina <reina5425@gmail.com>
2022-08-28 15:22:16 +0300

    update index.js

--- js/index.js
+++ js/index.js
@@ -8,27 +8,31 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 async function getData1(url) {
-	const response = await fetch(url);
-	const bigData = await response.json();
+	try {
+		const response = await fetch(url);
+		const bigData = await response.json();
 
-	const infoBlock = document.createElement('div');
-	infoBlock.classList.add('info-block');
+		const infoBlock = document.createElement('div');
+		infoBlock.classList.add('info-block');
 
-	const img = document.createElement('img');
-	img.classList.add('info-block__img');
+		const img = document.createElement('img');
+		img.classList.add('info-block__img');
 
-	const title = document.createElement('h2');
-	title.classList.add('info-block__title');
+		const title = document.createElement('h2');
+		title.classList.add('info-block__title');
 
-	const descr = document.createElement('p');
-	descr.classList.add('info-block__descr');
+		const descr = document.createElement('p');
+		descr.classList.add('info-block__descr');
 
-	mainBlock.append(infoBlock);
-	infoBlock.append(img);
-	infoBlock.append(title);
-	infoBlock.append(descr);
+		mainBlock.append(infoBlock);
+		infoBlock.append(img);
+		infoBlock.append(title);
+		infoBlock.append(descr);
 
-	img.src = bigData.image;
-	title.textContent = `Имя: ${bigData.name}`;
-	descr.textContent = `Вид: ${bigData.species}`;
+		img.src = bigData.image;
+		title.textContent = `Имя: ${bigData.name}`;
+		descr.textContent = `Вид: ${bigData.species}`;
+	} catch (err) {
+		console.log(err);
+	}
 }
